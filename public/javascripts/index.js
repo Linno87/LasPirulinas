f6bc1b9900ea11259ab5853c0814dde07eeb8493
Linno87 <linno824@gmail.com>
2024-01-24 00:40:11 -0300

    create: nav_search

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,5 @@
+
+/* Choce navbar with query */
 const burger_nav_phone = document.getElementById("nav_phone_menu");
 const burger_icon = document.getElementById("burger_icon")
 
@@ -9,5 +11,16 @@ burger_icon.addEventListener("click", () =>{
         burger_nav_phone.classList.remove("d-block")
         burger_nav_phone.classList.add("d-none")
         console.log("burger");
+    }
+})
+
+/* Open search bar */
+const input_search = document.getElementById("search_desk_input");
+const icon_search = document.getElementById("search_desk_icon");
+const form_seach = document.getElementById("form_search")
+icon_search.addEventListener("click",(e)=>{
+    if(!input_search.value.trim() || !input_search.classList.contains("search_desk_input_trans")){
+        e.preventDefault();
+        input_search.classList.toggle("search_desk_input_trans")
     }
 })
